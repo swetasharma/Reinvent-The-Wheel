fe9099b9fa11b318e4f3ac01fbfc980683555cdf
swetasharma <sweta.sharma@inin.com>
2017-02-23 23:48:32 +0530

    added problem statement - Bomber Algo

---
+++ src/BomberAlgo.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author Sweta Sharma
+ * The Bomber algo does not like 3 or more consecutive characters in a given string to be same
+ * For example: If the given string is "adbcccbbd" the bomber algo will first bomb "ccc" and then "bbb", so the final output string will become
+ * "add". The bomber algo will destroy the characters only if there are 3 or more coninuous occurence of same characters in string.
+ * Write a code of the bomber algo. The method takes a string as an input and returns bombed string as an output
+ * Input aaabcccdee
+ * Output bdee
+ * Input abcdeeeeddcbfgf
+ * Output abccbfgf
+ */
+
+public class BomberAlgo {
+
+}
