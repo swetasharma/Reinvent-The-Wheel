2762c88dfeadcf048db3acff60a381fe39d94462
swetasharma <sweta.sharma@inin.com>
2017-03-02 19:19:25 +0530

    Implemented Bomber algo need to fix exception part

--- src/BomberAlgo.java
+++ src/BomberAlgo.java
@@ -12,5 +12,51 @@
  */
 
 public class BomberAlgo {
-
+	public static void main(String[] args) {
+		char[] input = {'a','d', 'b', 'c', 'c', 'c', 'b', 'b', 'd'};
+		bombAlgo(input);	
+	}
+	public static void bombAlgo(char[] input){
+		while(input.length > 0){
+			int firstPointer  = 0;
+			int secondPointer = 1;
+			char[] arrRemovedRepetitiveCharacters = null;
+			while( firstPointer < input.length ){
+				if(input[firstPointer] != input[secondPointer]){
+					firstPointer  = firstPointer + 1;
+					secondPointer = secondPointer + 1;
+					if((secondPointer - firstPointer) > 1){
+						arrRemovedRepetitiveCharacters = removeConsecutiveCharacter(input, firstPointer, secondPointer - 1);
+						for(int i = 0; i < arrRemovedRepetitiveCharacters.length; i++){
+							System.out.print(arrRemovedRepetitiveCharacters[i]);
+						}
+						System.out.println("-----------------first round done------------");
+						break;
+					}
+				}else{
+					secondPointer = secondPointer + 1;
+				}						
+			}
+			input = arrRemovedRepetitiveCharacters;
+		}			
+	}
+	public static char[] removeConsecutiveCharacter(char[] input, int startPosition, int endPosition){
+		System.out.println("In removeconsecutive character");
+		int size = input.length - ((endPosition - startPosition) + 1);
+		//System.out.println(size + "The value of size is" + input.length + endPosition + startPosition);
+		char[] arrRemovedRepetitiveCharacters = new char[size];
+		int j = 0;
+		for(int i = 0; i < input.length; i++){
+			if(i < (startPosition - 1)){
+				arrRemovedRepetitiveCharacters[j] = input[i];
+				j = j + 1;
+			}else{
+				if( i >= (endPosition) ){
+					arrRemovedRepetitiveCharacters[j] = input[i];
+					j = j + 1;
+				}
+			}			
+		}	
+		return arrRemovedRepetitiveCharacters;
+	}	
 }
---
+++ src/FindSquareRootOfNumber.java
@@ -0,0 +1,27 @@
+
+public class FindSquareRootOfNumber {
+	public static void main(String[] args) {
+		int[] arrOfSquareNumbers = {1, 4, 9, 16, 25, 36, 49, 81, 100, 114};
+		int result = findSquareRootOfNumber(arrOfSquareNumbers, 16);
+	}
+	public static int findSquareRootOfNumber(int[] arrOfSquareNumbers, int num) {
+		return 0;
+		
+	}	
+}
+
+/*public static void main(String args[])
+{
+    byte a[] = { 65, 66, 67, 68, 69, 70 };
+    byte b[] = { 71, 72, 73, 74, 75, 76 };
+    System.arraycopy(b , 0, a, 0, a.length);
+    System.out.print(new String(a) + " " + new String(b));
+}*/
+ 
+/*public static void main(String args[])
+{
+    byte a[] = { 65, 66, 67, 68, 69, 70 };//A,B,C,D,E,F
+    byte b[] = { 71, 72, 73, 74, 75, 76 };//G,H,I,J,K,L
+    System.arraycopy(b , 0, a, 0, a.length);
+    System.out.print(new String(a) + " " + new String(b));
+}*/
